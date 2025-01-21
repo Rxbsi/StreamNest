3a8e7d0d8b66522f2298d0d2d118c5fb934522ca
Rxbsi <robinfranz02@gmail.com>
2025-01-21 17:53:05 +0100

    Adding frontend + backend, no logic

---
+++ dadad/src/main/java/de/rxbsi/streamnest/DadadApplication.java
@@ -0,0 +1,13 @@
+package de.rxbsi.streamnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadadApplication.class, args);
+    }
+
+}
