11ed7c4064bf24cbbedba936aa4d88e6d49a458e
Rxbsi <robinfranz02@gmail.com>
2025-01-21 17:53:05 +0100

    Adding frontend + backend, no logic

---
+++ dadad/src/test/java/de/rxbsi/streamnest/DadadApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rxbsi.streamnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
